67fd3a4330240fb42d875fa5624446a46cf17b59
uujeen <rkddmlwls2@gmail.com>
2024-11-15 18:36:48 +0900

    Style: 폰트 크기 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Root />}/>
-      <Route path='/' element={<Main />}/>
+      <Route path='/main' element={<Main />}/>
     </Routes>
   )
 }
